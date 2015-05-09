db430c2a6bdb151d5dcadfef1a1f7bf181813a6a
Rachel Margulies <rachelmrgls@gmail.com>
2015-05-09 00:03:55 -0400

    new stuff

---
+++ images/Noisy.js
@@ -0,0 +1,16 @@
+quantize = function() {
+    var c = document.getElementById("image");
+    var ctx = c.getContext("2d");
+    var img = document.getElementById("scream");
+    ctx.drawImage(img, 0, 0);
+    var imgData = ctx.getImageData(0, 0, c.width, c.height);
+    // invert colors
+    var i;
+    for (i = 0; i < imgData.data.length; i += 4) {
+        imgData.data[i] = 255 - imgData.data[i];
+        imgData.data[i+1] = 255 - imgData.data[i+1];
+        imgData.data[i+2] = 255 - imgData.data[i+2];
+        imgData.data[i+3] = 255;
+    }
+    ctx.putImageData(imgData, 0, 0);
+};
